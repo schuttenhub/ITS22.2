e959a3609b7b03321aa944bfe81180505eb58425
schuttenhub <106162570+schuttenhub@users.noreply.github.com>
2022-10-18 16:17:39 +0200

    yes

---
+++ sonstiges/Primzahlen_finder/primzahlen-finder.py
@@ -0,0 +1,10 @@
+"""counter = 1
+rest = 0
+print ("Bis zu welcher Zahl sollen Primzahlen errechnet werden?")
+maxzahl = int(input())"""
+
+
+#check ob Zahl durch alle Zahlen zwischen 1 und sich selbst restlos teilbar ist
+#schleife zum hochzählen bis maxzahl
+
+5 + 2
