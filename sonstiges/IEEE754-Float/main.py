a2da68146f7ff94f441e3d0b7d1b306ee223c1e8
schuttenhub <106162570+schuttenhub@users.noreply.github.com>
2022-10-22 22:32:27 +0200

    sync

--- sonstiges/IEEE754-Float/main.py
+++ sonstiges/IEEE754-Float/main.py
@@ -1,23 +1,27 @@
 #Umrechner von Dezimalzahlen in IEEE 754 Standard
-from pickle import APPEND
+def decimal_input():
+    decimal = float(input("Gib eine reelle Zahl ein: "))
 
+    if str(decimal)[0] == "-":
+        vorzeichen = 1
+        return abs(decimal)
+    else:
+        vorzeichen = 0
+        return decimal
 
-decimal = abs(float(input("Gib eine Zahl ein (R): ")))
-decimal_str = str(decimal)
+decimal_str = str(decimal_input())
 decimal_Ganz = int(decimal_str[:decimal_str.index(".")])
 decimal_DecP = int(decimal_str[decimal_str.index(".")+1:])
-
-
 
 ieee = int(0)
 
 def Decimal_GanzInBinary(decimal_Ganz):
-    binarystr = str()
+    binary_Ganz = str()
     while decimal_Ganz > 0:
-        binarystr += str(int(decimal_Ganz % 2))
+        binary_Ganz += str(int(decimal_Ganz % 2))
         decimal_Ganz = decimal_Ganz // 2
-    binarystr = binarystr[::-1]
-    return binarystr
+    binary_Ganz = binary_Ganz[::-1]
+    return binary_Ganz
 
 def Decimal_DecPInBinary(decimal_DecP):
     decimal_DecP = float(str("0.") + str(decimal_DecP))
@@ -35,25 +39,27 @@ def Decimal_DecPInBinary(decimal_DecP):
             binary_DecP += "0"
     return binary_DecP
 
-
-def BinaryInIEEE(decimal, binary):
-    #vorzeichen = 0
-    #ieee = 0
-    #ieeestr = ()
-    #if decimal == 0:
-    #    ieee = 00000000000000000000000000000000
-    #    return ieee
-    #if decimal > 0:
-    #    vorzeichen = 0
-    #else:
-    #    vorzeichen = 1
-    #
-    #while True:
-    pass
-    #return ieee
-
-
-
 binary_Ganz = Decimal_GanzInBinary(decimal_Ganz)
 binary_DecP = Decimal_DecPInBinary(decimal_DecP)
-print("{}.{}".format(binary_Ganz,binary_DecP))
+output_before_IEEE = "{}.{}".format(binary_Ganz,binary_DecP)
+
+print(output_before_IEEE)
+
+#def BinaryInIEEE(vorzeichen, binary_Ganz, binary_DecP,output_before_IEEE):
+#    vorzeichen = 0
+#    ieee = 0
+#    ieeestr = ()
+#    if decimal == 0:
+#        ieee = 00000000000000000000000000000000
+#        return ieee
+#    if decimal > 0:
+#        vorzeichen = 0
+#    else:
+#        vorzeichen = 1
+#    
+#    while True:
+#    pass
+#
+
+
+
