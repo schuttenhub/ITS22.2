7db738394cc19686696c83af4805aea3799195a3
schuttenhub <106162570+schuttenhub@users.noreply.github.com>
2022-10-25 23:08:13 +0200

    updates

--- 11_Fachsemester1/12000_Programmierung1/Aufgabenblaetter/_garbage.py
+++ 11_Fachsemester1/12000_Programmierung1/Aufgabenblaetter/_garbage.py
@@ -1,23 +1,72 @@
-#3.1 b)
-from decimal import Decimal
-from math import sqrt
-nachkomma = 10
-vergleiche_mit_pi = 15_000_000
+#Wortgenerator
+from operator import le
+import string
 
-def pi_calc(nachkomma, vergleiche_mit_pi):
-    i = 1
-    i2 = 0
-    pi_vgl = Decimal(0)
-    pi_org = Decimal(0)
-    while True:
-        pi_vgl = round(pi_org, nachkomma)
-        pi_org = pi_org + Decimal(1) / Decimal(i**2)
-        i += 1
-        if pi_vgl == round(pi_org, nachkomma):
-            i2 += 1
-            if i2 == vergleiche_mit_pi:
-                return Decimal(sqrt(pi_org * Decimal(6)))
+def wort_generator():
+    letters = string.ascii_lowercase
+    word = 'aaaa'
+    i_1 = ''
+    i_2 = ''
+    i_3 = ''
+    i_4 = ''
+
+    
+    for i_1 in letters:
+        word = i_1 + i_2 + i_3 + i_4
+        if (wort_check_1(word) and wort_check_2(word)):
+            print(word)
+
+        for i_2 in letters:
+            word = i_1 + i_2 + i_3 + i_4
+            if (wort_check_1(word) and wort_check_2(word)):
+                print(word)
+
+            for i_3 in letters:
+                word = i_1 + i_2 + i_3 + i_4
+                if (wort_check_1(word) and wort_check_2(word)):
+                    print(word)
+
+                for i_4 in letters:
+                    word = i_1 + i_2 + i_3 + i_4
+                    if (wort_check_1(word) and wort_check_2(word)):
+                        print(word)
+
+                i_4 = letters[0]
+            i_3 = letters[0]
+        i_2 = letters[0]
 
 
-print(pi_calc(nachkomma, vergleiche_mit_pi))
-#Mehr als 6 oder 7 korrekte Nachkommastellen sind kaum in absehbarer Zeit möglich (+/- 30 Sekunden)
+def wort_check_1(word):
+    konsonanten = 'bcdfghjklmnpqrstvwxyz'
+    if len(word) <=3: #nur damit auch mindestens 4 Zeichen ausgegeben werden
+        return False
+
+    let_in_word = 0
+    kons_count = 0
+    for x in range(3):
+        for i_check in konsonanten:
+            if i_check == word[let_in_word]:
+                let_in_word +=1
+                kons_count +=1
+                break
+    if kons_count >= 3:
+        return False
+    else:
+        return True
+
+def wort_check_2(word):
+    konsonanten = 'bcdfghjklmnpqrstvwxyz'
+    let_in_word = 1
+    kons_count = 0
+    for x in range(3):
+        for i_check in konsonanten:
+            if i_check == word[let_in_word]:
+                let_in_word += 1
+                kons_count += 1
+                break
+    if kons_count >= 3:
+        return False
+    else:
+        return True
+
+wort_generator()
