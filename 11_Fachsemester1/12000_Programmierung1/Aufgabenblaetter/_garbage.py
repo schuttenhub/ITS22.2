82cdb5eee174c2adcdd11280cbda3a081619fc35
schuttenhub <106162570+schuttenhub@users.noreply.github.com>
2022-10-27 08:00:32 +0200

    updates

--- 11_Fachsemester1/12000_Programmierung1/Aufgabenblaetter/_garbage.py
+++ 11_Fachsemester1/12000_Programmierung1/Aufgabenblaetter/_garbage.py
@@ -1,54 +1,9 @@
-#Wortgenerator
-from operator import le
-import string
+d = {0:'Hallo', 'Hallo':2, 2:'Hallo', 3:4}
 
-def wort_generator():
-    letters = string.ascii_lowercase
-    word = ''
-    i_1 = ''
-    i_2 = ''
-    i_3 = ''
-    i_4 = ''
+d_keys = list(d.keys())
 
-    for i_1 in letters:
-        for i_2 in letters:
-            for i_3 in letters:
-                for i_4 in letters:
-                    word = i_1 + i_2 + i_3 + i_4
-                    if (wort_check_1(word) and wort_check_2(word)):
-                        print(word)
+print(type(d_keys[0]))
 
-def wort_check_1(word):
-    konsonanten = 'bcdfghjklmnpqrstvwxyz'
-    if len(word) <=3: #nur damit auch mindestens 4 Zeichen ausgegeben werden
-        return False
-
-    let_in_word = 0
-    kons_count = 0
-    for x in range(3):
-        for i_check in konsonanten:
-            if i_check == word[let_in_word]:
-                let_in_word +=1
-                kons_count +=1
-                break
-    if kons_count >= 3:
-        return False
-    else:
-        return True
-
-def wort_check_2(word):
-    konsonanten = 'bcdfghjklmnpqrstvwxyz'
-    let_in_word = 1
-    kons_count = 0
-    for x in range(3):
-        for i_check in konsonanten:
-            if i_check == word[let_in_word]:
-                let_in_word += 1
-                kons_count += 1
-                break
-    if kons_count >= 3:
-        return False
-    else:
-        return True
-
-wort_generator()
+for i in range(len(d)):
+    if d_keys[i] is type(int):
+        print(f"Key {i}-{d_keys[i]} is type int")
