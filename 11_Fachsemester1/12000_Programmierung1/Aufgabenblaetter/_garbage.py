703a5b79dd892f7f7e81c24cfc196fe07bdf0faa
schuttenhub <106162570+schuttenhub@users.noreply.github.com>
2022-10-31 14:01:20 +0100

    updates

--- 11_Fachsemester1/12000_Programmierung1/Aufgabenblaetter/_garbage.py
+++ 11_Fachsemester1/12000_Programmierung1/Aufgabenblaetter/_garbage.py
@@ -1,9 +1,10 @@
-d = {0:'Hallo', 'Hallo':2, 2:'Hallo', 3:4}
+#Aufgabe 3.3 c)
+d = {0:'Hallo', 'Hallo':2, 2:'Hallo', 3:[4, 5, 6], }
 
 d_keys = list(d.keys())
+i = 0
+while type(d_keys[i]) == int:
+    d_keys_is_int = True
+    i += 1
 
-print(type(d_keys[0]))
-
-for i in range(len(d)):
-    if d_keys[i] is type(int):
-        print(f"Key {i}-{d_keys[i]} is type int")
+print(f"Alle Schlüssel sind int -> {d_keys_is_int}")
