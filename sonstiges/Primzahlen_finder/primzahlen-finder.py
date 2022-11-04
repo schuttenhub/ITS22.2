732e12f64cb167dc883c8f3927fb24f8e1e0de04
schuttenhub <mschuttenberg@posteo.de>
2022-11-04 22:46:47 +0100

    editing

--- sonstiges/Primzahlen_finder/primzahlen-finder.py
+++ sonstiges/Primzahlen_finder/primzahlen-finder.py
@@ -1,20 +1,25 @@
-#Primzahlen Finder
-
-prim_max = 100
-
+#Aufgabe 4.0 aber es werden Primzahlen bis zu einem eingegebenen Limit gerechnet
+prim_calc_limit = int(input('Bitte Zahl eingeben '))
 
 
-def prim_finder(prim_max):
-    primzahl = 0
-    while primzahl <= prim_max:
-#        for i in range((primzahl//2)):
-#            i += 1
-#            if primzahl % i == 0:
-#                print(primzahl)
-#        primzahl += 1            
+def prim(maybe_prim):
+    prim_index = 0
+    if maybe_prim == 0: return False
+    if maybe_prim == 1: return False
 
 
+    for i in range(maybe_prim):       
+        rest = maybe_prim % (i+1)
+        if rest == 0:
+            prim_index += 1
+        if prim_index > 2:
+            return False
+        if (maybe_prim > 20) and (i > ((maybe_prim//2) + 1) ): break
 
+    
+    return maybe_prim  
 
-
-prim_finder(prim_max)
+for i in range(prim_calc_limit):
+    print_filter = prim(i)
+    if type(print_filter) == int:
+        print(print_filter)
